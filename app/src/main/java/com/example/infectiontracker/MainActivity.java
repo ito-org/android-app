385ea668470129bd2031a4e3819f4a86a9568fcb
hermlon <dev.hermlon@yamx.net>
2020-03-21 14:00:38 +0100

    Permission explaination layout

--- app/src/main/java/com/example/infectiontracker/MainActivity.java
+++ app/src/main/java/com/example/infectiontracker/MainActivity.java
@@ -1,18 +1,54 @@
 package com.example.infectiontracker;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final int MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Intent intent = new Intent(this, TracingService.class);
-        startService(intent);
+        //checkPermissions();
     }
+
+    private void checkPermissions() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            // Permission is not granted
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // Start background service
+                    Intent intent = new Intent(this, TracingService.class);
+                    startService(intent);
+                } else {
+                    // Don't start the discovery service
+                    //TODO: Hint in UI to request permissions again
+                }
+                return;
+            }
+        }
+    }
+
 }
