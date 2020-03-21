cc6a3a7ff7e71d77c0be5bef09bab7725cecd24b
Martin <martin@urbackup.org>
2020-03-21 20:37:01 +0100

    Get contact information from intent

--- app/src/main/java/com/example/infectiontracker/InfectedInfo.java
+++ app/src/main/java/com/example/infectiontracker/InfectedInfo.java
@@ -9,6 +9,9 @@ import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class InfectedInfo extends AppCompatActivity {
+    public static final String INTENT_CONTACT_TIME = "contact_time";
+    public static final String INTENT_CONTACT_DURATION = "contact_duration";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,5 +19,10 @@ public class InfectedInfo extends AppCompatActivity {
         TextView tv = (TextView)findViewById(R.id.infectedInfoLink);
         tv.setText(Html.fromHtml(getString(R.string.infected_info_link)));
         tv.setMovementMethod(LinkMovementMethod.getInstance());
+
+        Intent intent = getIntent();
+
+        ((TextView)findViewById(R.id.approxContactTime)).setText(intent.getStringExtra(INTENT_CONTACT_TIME) );
+        ((TextView)findViewById(R.id.approxContactDuration)).setText(intent.getStringExtra(INTENT_CONTACT_DURATION) );
     }
 }
