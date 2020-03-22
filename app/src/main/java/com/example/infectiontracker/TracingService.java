02772e4f872e9678ef8130a6910c7a06cc52dc4f
hermlon <dev.hermlon@yamx.net>
2020-03-22 13:37:43 +0100

    Show actual infection information

--- app/src/main/java/com/example/infectiontracker/TracingService.java
+++ app/src/main/java/com/example/infectiontracker/TracingService.java
@@ -209,6 +209,7 @@ public class TracingService extends Service {
                 // TODO
             }
         };
+        // TODO: check if null when Bluetooth disabled
         bluetoothLeAdvertiser.startAdvertising(settings, data, advertisingCallback);
     }
 
--- app/src/main/java/com/example/infectiontracker/viewmodel/InfectionCheckViewModel.java
+++ app/src/main/java/com/example/infectiontracker/viewmodel/InfectionCheckViewModel.java
@@ -3,6 +3,7 @@ package com.example.infectiontracker.viewmodel;
 import android.app.Application;
 
 import com.example.infectiontracker.database.InfectedUUID;
+import com.example.infectiontracker.database.Infection;
 import com.example.infectiontracker.repository.InfectedUUIDRepository;
 
 import java.util.List;
@@ -14,7 +15,7 @@ public class InfectionCheckViewModel extends AndroidViewModel {
 
     private InfectedUUIDRepository mRepository;
 
-    private LiveData<List<InfectedUUID>> possiblyInfectedEncounters;
+    private LiveData<List<Infection>> possiblyInfectedEncounters;
 
     public InfectionCheckViewModel(Application application) {
         super(application);
@@ -26,6 +27,6 @@ public class InfectionCheckViewModel extends AndroidViewModel {
         mRepository.refreshInfectedUUIDs();
     }
 
-    public LiveData<List<InfectedUUID>> getPossiblyInfectedEncounters() { return possiblyInfectedEncounters; }
+    public LiveData<List<Infection>> getPossiblyInfectedEncounters() { return possiblyInfectedEncounters; }
 
 }
