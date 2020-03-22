fad261f5544c059d56671603ca96f831d79a7f79
hermlon <dev.hermlon@yamx.net>
2020-03-22 11:39:46 +0100

    showing encountered infections

--- app/src/main/java/com/example/infectiontracker/ui/InfectedUUIDsAdapter.java
+++ app/src/main/java/com/example/infectiontracker/ui/InfectedUUIDsAdapter.java
@@ -1,5 +1,6 @@
 package com.example.infectiontracker.ui;
 
+import android.text.format.DateFormat;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
@@ -8,6 +9,7 @@ import android.widget.TextView;
 import com.example.infectiontracker.R;
 import com.example.infectiontracker.database.InfectedUUID;
 
+import java.util.Collections;
 import java.util.List;
 
 import androidx.annotation.NonNull;
@@ -15,15 +17,20 @@ import androidx.recyclerview.widget.RecyclerView;
 
 public class InfectedUUIDsAdapter extends RecyclerView.Adapter<InfectedUUIDsAdapter.InfectedUUIDsViewHolder> {
 
-    private List<InfectedUUID> infectedUUIDs;
+    private List<InfectedUUID> infectedUUIDs = Collections.emptyList();
 
     public static class InfectedUUIDsViewHolder extends RecyclerView.ViewHolder {
         public LinearLayout layout;
-        public TextView textView;
+        public TextView textViewDate;
+        public TextView textViewDisease;
+        public TextView textViewDanger;
+
         public InfectedUUIDsViewHolder(LinearLayout v) {
             super(v);
             layout = v;
-            textView = v.findViewById(R.id.infected_list_text_view);
+            textViewDate = v.findViewById(R.id.infected_list_text_view_date);
+            textViewDisease = v.findViewById(R.id.infected_list_text_view_disease);
+            textViewDanger = v.findViewById(R.id.infected_list_text_view_danger);
         }
     }
 
@@ -38,7 +45,15 @@ public class InfectedUUIDsAdapter extends RecyclerView.Adapter<InfectedUUIDsAdap
 
     @Override
     public void onBindViewHolder(@NonNull InfectedUUIDsViewHolder holder, int position) {
-        holder.textView.setText("Test " + infectedUUIDs.get(position).icdCode);
+        InfectedUUID data = infectedUUIDs.get(position);
+
+        java.text.DateFormat df = DateFormat.getDateFormat(holder.layout.getContext());
+        // TODO: change to actual date from Beacons Table
+        holder.textViewDate.setText(df.format(data.createdOn));
+        holder.textViewDisease.setText(data.icdCode);
+        // TODO: this isn't actually the danger yet
+        holder.textViewDanger.setText(Integer.toString(data.distrustLevel));
+        // TODO: add nearest distance
     }
 
     @Override
