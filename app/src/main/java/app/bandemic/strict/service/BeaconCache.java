36328c99b980cc134f131da233de46d32c347f7e
Christian Romberg <distjubo@gmail.com>
2020-03-22 17:36:42 +0100

    Implement BeaconCache for more acccurate positioning

---
+++ app/src/main/java/app/bandemic/strict/service/BeaconCache.java
@@ -0,0 +1,97 @@
+package app.bandemic.strict.service;
+
+import android.os.Handler;
+import android.util.Log;
+
+import androidx.collection.CircularArray;
+
+import java.util.Date;
+import java.util.HashMap;
+
+import app.bandemic.strict.database.Beacon;
+import app.bandemic.strict.repository.BroadcastRepository;
+import okio.ByteString;
+
+public class BeaconCache {
+    private static final String LOG_TAG = "BeaconCache";
+    private final int MOVING_AVERAGE_LENGTH = 15;
+    private final long FLUSH_AFTER_MILLIS = 1000 * 60 * 3; // flush after three minutes
+
+    private BroadcastRepository broadcastRepository;
+    private Handler serviceHandler;
+    private HashMap<ByteString, CacheEntry> cache = new HashMap<>();
+
+    public BeaconCache(BroadcastRepository broadcastRepository, Handler serviceHandler) {
+        this.broadcastRepository = broadcastRepository;
+        this.serviceHandler = serviceHandler;
+    }
+
+    private void flush(ByteString hash) {
+        Log.d(LOG_TAG, "Flushing distance to DB");
+        CacheEntry entry = cache.get(hash);
+        CircularArray<Double> distances = entry.distances;
+        double avg = 0;
+        for (int i = 0; i < distances.size(); i++) {
+            avg += distances.get(i) / distances.size();
+        }
+        if (avg < entry.lowestDistance) {
+            entry.lowestDistance = avg;
+            insertIntoDB(entry.hash, avg);
+        }
+        cache.remove(hash);
+    }
+
+    public void flush() {
+        for (ByteString hash : cache.keySet()) {
+            flush(hash);
+        }
+    }
+
+    public void addReceivedBroadcast(byte[] hash, double distance) {
+        ByteString hashString = ByteString.of(hash);
+        CacheEntry entry = cache.get(hashString);
+
+        if (entry == null) {
+            // new unknown broadcast
+            entry = new CacheEntry();
+            cache.put(hashString, entry);
+            entry.hash = hash;
+        }
+
+        // postpone flushing
+        serviceHandler.removeCallbacks(entry.flushRunnable);
+        serviceHandler.postDelayed(entry.flushRunnable, FLUSH_AFTER_MILLIS);
+
+        CircularArray<Double> distances = entry.distances;
+        distances.addFirst(distance);
+        if (distances.size() == MOVING_AVERAGE_LENGTH) {
+
+            //calculate moving average
+            double avg = 0;
+            for (int i = 0; i < MOVING_AVERAGE_LENGTH; i++) {
+                avg += distances.get(i) / MOVING_AVERAGE_LENGTH;
+            }
+            if (avg < entry.lowestDistance) {
+                //insert new lowest value to DB
+                entry.lowestDistance = avg;
+                insertIntoDB(hash, avg);
+            }
+            distances.popLast();
+        }
+    }
+
+    private void insertIntoDB(byte[] hash, double distance) {
+        broadcastRepository.insertBeacon(new Beacon(
+                hash,
+                new Date(System.currentTimeMillis()),
+                distance
+        ));
+    }
+
+    private class CacheEntry {
+        byte[] hash;
+        CircularArray<Double> distances = new CircularArray<>(MOVING_AVERAGE_LENGTH);
+        double lowestDistance = Double.MAX_VALUE;
+        Runnable flushRunnable = () -> flush(ByteString.of(hash));
+    }
+}
