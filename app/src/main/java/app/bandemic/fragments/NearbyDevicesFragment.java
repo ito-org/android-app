fc86cf6281fdc4f5773844c5a14bf2c68c9d410a
Anton Schirg <anton.schirg@posteo.de>
2020-03-30 17:10:58 +0200

    Visualize nearby devices as "floating bubbles"

--- app/src/main/java/app/bandemic/fragments/NearbyDevicesFragment.java
+++ app/src/main/java/app/bandemic/fragments/NearbyDevicesFragment.java
@@ -9,25 +9,27 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.cardview.widget.CardView;
+import androidx.dynamicanimation.animation.DynamicAnimation;
+import androidx.dynamicanimation.animation.SpringAnimation;
+import androidx.dynamicanimation.animation.SpringForce;
 import androidx.fragment.app.Fragment;
 import androidx.localbroadcastmanager.content.LocalBroadcastManager;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import app.bandemic.R;
-import app.bandemic.ui.NearbyDevicesAdapter;
 
 public class NearbyDevicesFragment extends Fragment {
 
-    private RecyclerView recyclerView;
-    private NearbyDevicesAdapter mAdapter;
-    private LinearLayout noInfectionInformation;
-    private CardView cardView;
+    private RelativeLayout layout;
+
+    private List<NearbyDeviceView> nearbyDeviceViews = new ArrayList<>();
 
 
     @Override
@@ -39,21 +41,80 @@ public class NearbyDevicesFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        recyclerView = view.findViewById(R.id.environment_logger_list_recycler_view);
-        noInfectionInformation = view.findViewById(R.id.layout_no_detections);
-        recyclerView.setHasFixedSize(true);
-        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getActivity());
-        recyclerView.setLayoutManager(layoutManager);
-        mAdapter = new NearbyDevicesAdapter();
-        recyclerView.setAdapter(mAdapter);
-        cardView = view.findViewById(R.id.environmentCard);
+
+        layout = view.findViewById(R.id.layout);
+    }
+
+    private class NearbyDeviceView {
+        ImageView iv;
+        SpringAnimation animX;
+        SpringAnimation animY;
+    }
+
+    private void addNearbyDevice() {
+        ImageView newIV = new ImageView(this.getContext());
+        newIV.setImageDrawable(getResources().getDrawable(R.drawable.nearby_device));
+        RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        layoutParams.topMargin = 0;
+        layoutParams.leftMargin = 0;
+        layoutParams.addRule(RelativeLayout.ALIGN_LEFT, R.id.nearby_device_myself);
+        layoutParams.addRule(RelativeLayout.ALIGN_TOP, R.id.nearby_device_myself);
+        layout.addView(newIV, layoutParams);
+
+        final SpringAnimation anim1X = new SpringAnimation(newIV,
+                DynamicAnimation.TRANSLATION_X);
+        final SpringAnimation anim1Y = new SpringAnimation(newIV,
+                DynamicAnimation.TRANSLATION_Y);
+        anim1X.setSpring(new SpringForce().setStiffness(SpringForce.STIFFNESS_LOW));
+        anim1Y.setSpring(new SpringForce().setStiffness(SpringForce.STIFFNESS_LOW));
+
+        NearbyDeviceView ndv = new NearbyDeviceView();
+        ndv.iv = newIV;
+        ndv.animX = anim1X;
+        ndv.animY = anim1Y;
+
+        nearbyDeviceViews.add(ndv);
+    }
+
+    private void removeNearbyDevice() {
+        if (nearbyDeviceViews.size() > 0) {
+            NearbyDeviceView removed = nearbyDeviceViews.remove(0);
+            removed.animX.cancel();
+            removed.animY.cancel();
+            layout.removeView(removed.iv);
+        }
+    }
+
+    private void updateDevicePositions(double[] distances) {
+        if (nearbyDeviceViews.size() < distances.length) {
+            addNearbyDevice();
+        }
+        if (nearbyDeviceViews.size() > distances.length) {
+            removeNearbyDevice();
+        }
+
+        double angle_min = 0f / 180f * Math.PI;
+        double angle_max = Math.PI - angle_min;
+        double angle_range = angle_max - angle_min;
+
+        for (int i = 0; i < nearbyDeviceViews.size(); i++) {
+            double angle = angle_min + (angle_range / (nearbyDeviceViews.size() + 1)) * (i + 1);
+            double min_distance_on_screen = 100d;
+            double max_distance_on_screen = 300d;
+            double distance_scaling = 3d; // Make bigger to make effect of actual distance stronger in near range
+            double distance = max_distance_on_screen - (1d/((distances[i]/ distance_scaling)+1d) * (max_distance_on_screen - min_distance_on_screen));
+            double top = -Math.sin(angle) * distance;
+            double left = Math.cos(angle) * distance;
+            NearbyDeviceView ndv = nearbyDeviceViews.get(i);
+            ndv.animX.animateToFinalPosition((float) left);
+            ndv.animY.animateToFinalPosition((float) top);
+        }
     }
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 
-        cardView.setCardBackgroundColor(getResources().getColor(R.color.colorNoDanger));
     }
 
     @Override
@@ -68,18 +129,7 @@ public class NearbyDevicesFragment extends Fragment {
         public void onReceive(Context context, Intent intent) {
             double[] distances = intent.getDoubleArrayExtra("distances");
 
-            if(distances.length != 0) {
-                mAdapter.setDistances(distances);
-                noInfectionInformation.setVisibility(View.GONE);
-                recyclerView.setVisibility(View.VISIBLE);
-                cardView.setCardBackgroundColor(getResources().getColor(R.color.colorDanger));
-            }
-            else {
-                noInfectionInformation.setVisibility(View.VISIBLE);
-                recyclerView.setVisibility(View.GONE);
-                cardView.setCardBackgroundColor(getResources().getColor(R.color.colorNoDanger));
-
-            }
+            updateDevicePositions(distances);
         }
     };
 
